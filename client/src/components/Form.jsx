8bd6d190755c1295da43f7b3fe5d95184f98669a
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-02-27 18:51:44 -0800

    Add form, formInput, and basic styles

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import FormInput from './FormInput';
+
+const initialFormInput = {
+  name: '',
+  album: '',
+};
+
+const Form = function CreateForm ({ handleSubmit }) {
+  const [ formInput, setFormInput ] = useState(initialFormInput);
+
+  const handleInputChange = () => {
+    setFormInput({
+      ...formInput,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  return (
+    <StyledForm
+      onSubmit={() => {
+        handleSubmit(formInput)
+      }}
+    >
+      <h3>Enter your pick!</h3>
+      <div>
+        <FormInput
+          labelText="Name"
+          type="text"
+          name="name"
+          value={formInput.name}
+          placeholder="Enter name"
+          onChange={handleInputChange}
+        />
+        <FormInput
+          labelText="Album"
+          type="text"
+          name="album"
+          value={formInput.album}
+          placeholder="Enter album"
+          onChange={handleInputChange}
+        />
+        <input type="submit" value="Register"></input>
+      </div>
+    </StyledForm>
+  );
+};
+
+const StyledForm = styled.form`
+  font-family: 'Recursive', sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  row-gap: 8px;
+`;
+
+export default Form;
